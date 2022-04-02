3ccbb9858d7dd8d06a0dd142fa164cde23e58a43
Nicolas Zapata <91705544+Nicolaszap@users.noreply.github.com>
2022-04-01 22:39:08 -0500

    Add files via upload

---
+++ Mayor y menor.py
@@ -0,0 +1,11 @@
+numero1 = input("Ingrese un numero : ")
+numero2 = input("Ingrese otro numero :")
+
+if numero1 > numero2:
+     print("El numero " + numero1 + " es mayor que " + numero2)
+elif numero1 < numero2:
+    print("El numero " + numero1 + " es menor que " + numero2)
+elif numero1 == numero2:
+    print("El numero " + numero1 + " es igual que " + numero2)
+else:
+    print(" Ingresar datos validos")
---
+++ tabla de multiplicar.py
@@ -0,0 +1,6 @@
+
+numero = input("escriba un numero: ")
+
+for i in range (1,11):
+    #print (str(numero) +" x " + str(i) +" = "+ str(numero*i))
+    print (numero +" x " + i +" = "+ numero*i)
